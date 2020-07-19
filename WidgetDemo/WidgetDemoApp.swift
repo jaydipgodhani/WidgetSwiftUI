a4e793668755e562444af7539f6721bdb7540a21
Jaydip Godahni <godhani.jaydip145@gmail.com>
2020-07-19 17:12:16 +0530

    Initial Commit

---
+++ WidgetDemo/WidgetDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  WidgetDemoApp.swift
+//  WidgetDemo
+//
+//  Created by Rahul Patel on 19/07/20.
+//
+
+import SwiftUI
+
+@main
+struct WidgetDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
